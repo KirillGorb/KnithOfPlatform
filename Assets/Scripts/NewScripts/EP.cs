2bf2b4c5889bdb1956be5abf68de09172ecd1ffd
desmit <57844277+desmit@users.noreply.github.com>
2023-03-05 16:23:41 +0200

    05/03
    New Script EX and  Enemy
    New location

---
+++ Assets/Scripts/NewScripts/EP.cs
@@ -0,0 +1,82 @@
+﻿public class Character : MonoBehaviour
+{
+    public int level = 1;
+    public int experience = 0;
+    public int experienceNeeded = 100;
+    public int maxLevel = 99;
+    public int maxEnemies = 100;
+    public float health = 100f;
+    public float maxHealth = 100f;
+    public float healthRegenRate = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Health regeneration
+        if (health < maxHealth)
+        {
+            health += healthRegenRate * Time.deltaTime;
+            health = Mathf.Clamp(health, 0f, maxHealth);
+        }
+    }
+
+    public void AddExperience(int amount)
+    {
+        experience += amount;
+
+        // Check if level up
+        if (experience >= experienceNeeded)
+        {
+            level++;
+            experience -= experienceNeeded;
+
+            // Increase health and add new abilities
+            maxHealth += 10f;
+            health = maxHealth;
+            AddAbilities();
+
+            // Increase experience needed for next level
+            experienceNeeded = Mathf.FloorToInt(experienceNeeded * 1.1f);
+            experienceNeeded = Mathf.Clamp(experienceNeeded, 0, int.MaxValue);
+
+            // Check if max level reached
+            if (level > maxLevel)
+            {
+                level = maxLevel;
+                experience = 0;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            // Reduce health when touching an enemy
+            health -= 10f;
+
+            if (health <= 0f)
+            {
+                // Reset level and experience when character dies
+                level = 1;
+                experience = 0;
+                health = maxHealth;
+            }
+        }
+    }
+
+    private void AddAbilities()
+    {
+        // Add new abilities for each level
+        switch (level)
+        {
+            case 2:
+                // Add new ability for level 2
+                break;
+            case 3:
+                // Add new ability for level 3
+                break;
+                // Add more cases for additional levels and abilities
+        }
+    }
+}
---
+++ Assets/Scripts/NewScripts/Enemies.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform characterTransform;
+    public float spawnRadius = 10f;
+    public float enemySpeed = 5f;
+    public int maxEnemies = 5;
+
+    private List<GameObject> enemies = new List<GameObject>();
+
+    void Update()
+    {
+        // Check if we need to spawn a new enemy
+        if (enemies.Count < maxEnemies)
+        {
+            SpawnEnemy();
+        }
+
+        // Remove any dead enemies
+        for (int i = enemies.Count - 1; i >= 0; i--)
+        {
+            if (enemies[i] == null)
+            {
+                enemies.RemoveAt(i);
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 spawnPosition = Random.insideUnitCircle * spawnRadius;
+        spawnPosition += characterTransform.position;
+        GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        enemies.Add(enemy);
+        EnemyMovement enemyMovement = enemy.GetComponent<EnemyMovement>();
+        enemyMovement.characterTransform = characterTransform;
+        enemyMovement.speed = enemySpeed;
+    }
+}
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform characterTransform;
+    public float speed = 5f;
+
+    void Update()
+    {
+        Vector3 direction = characterTransform.position - transform.position;
+        direction.Normalize();
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
