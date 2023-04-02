a44c927702bb78faad9c95204403573c47114927
desmit <57844277+desmit@users.noreply.github.com>
2023-04-02 17:41:26 +0300

    new 2 Script
    2

---
+++ Assets/Scripts/NewScripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5f; 
+    private Rigidbody2D rb; 
+
+    private Transform playerTransform; 
+    private Vector2 direction; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+    void Update()
+    {        
+        direction = playerTransform.position - transform.position;
+        direction.Normalize(); 
+
+        
+        if (direction.x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); 
+        }
+        else if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(direction.x * speed * Time.fixedDeltaTime, 0);
+        rb.MovePosition(rb.position + movement);
+    }
+}
---
+++ Assets/Scripts/NewScripts/HealthScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void SetHealth(int newHealth)
+    {
+        health = Mathf.Min(newHealth, maxHealth);
+    }
+
+    private void Die()
+    {
+        // Add code for when the object dies here
+    }
+}
+
