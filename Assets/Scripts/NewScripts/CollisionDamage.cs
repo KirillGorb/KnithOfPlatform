9a3d843e8c427601f016ba0d38f8680afcf2314c
desmit <57844277+desmit@users.noreply.github.com>
2023-04-07 02:41:18 +0300

    //

---
+++ Assets/Scripts/NewScripts/CollisionDamage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamage : MonoBehaviour
+{
+    public int collisionDamage;
+    public string collisionTag;
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == collisionTag)
+        {
+            HealthScript health = coll.gameObject.GetComponent<HealthScript>();
+            if (health != null)
+            {
+                health.TakeDamage(collisionDamage);
+            }
+        }
+    }
+}
